325faefcd81b0259c09d9291e5c2f8d581a60a8c
Madhav12-cloud <madhavwagatkar123456789@gmail.com>
2026-04-30 08:22:38 +0530

    use env variable for mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     // await mongoose.connect('mongodb+srv://proj_db_user:invoice123@cluster0.htu6gtu.mongodb.net/InvoiceAI')
-    await mongoose.connect('mongodb+srv://maddy1997ac_db_user:invoice123@cluster0.gq8aigs.mongodb.net/InvoiceAI')
+    // await mongoose.connect('mongodb+srv://maddy1997ac_db_user:invoice123@cluster0.gq8aigs.mongodb.net/InvoiceAI')
+    await mongoose.connect(process.env.MONGODB_URI)
     .then(() => {console.log("DB Connected")})
 } 
